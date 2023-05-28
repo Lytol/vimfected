ec8e317b234d47a20a6e189866212a69115b893c
Brian Smith <brian.e.smith@gmail.com>
2023-05-28 15:50:31 -0700

    Initial import of Vite + typescript + phaser

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+import Example from './scenes/Example';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: Example
+};
+
+export default new Phaser.Game(config);
---
+++ src/scenes/Example.ts
@@ -0,0 +1,13 @@
+export default class Example extends Phaser.Scene {
+    constructor () {
+        super();
+    }
+
+    preload () {
+
+    }
+
+    create () {
+        console.log("DEBUG");
+    }
+}
